1796869b2afb613a034de922ca0d27a4f81c0552
Paulo Santos <paulo_sss@hotmail.com>
2017-02-17 16:41:23 +0000

    rearrages folders and adds 02_pacckage

---
+++ 02_package/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/pawlo0/udemyGolangTraining/02_package/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+}
